62c2c66935653057b87fb7b6aeb3a5fd9df60e05
Chris <cmagerabronn110@web.de>
2015-04-04 20:57:46 +0200

    added coupon model

--- POS/POS/Models/Coupon.cs
+++ POS/POS/Models/Coupon.cs
@@ -4,10 +4,19 @@ namespace POS.Models
 {
     public class Coupon
     {
+        public enum CouponType
+        {
+            Free,
+            Discount,
+            Special
+        }
+
         public string Code { get; set; }
         public bool IsValid { get { return Validate(this); } }
         public DateTime ExpireDate { get; set; }
         public bool IsExpired { get { return DateTime.Today <= ExpireDate; } }
+        public CouponType Type { get; set; }
+        public decimal Value { get; set; }
 
         public static Coupon NewCoupon()
         {
