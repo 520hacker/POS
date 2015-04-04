12eb45d3b333da34b27932a18a8dc356287aa709
Chris <cmagerabronn110@web.de>
2015-04-04 20:31:56 +0200

    adde models
    added product & coupon model

---
+++ POS/POS/Models/Coupon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace POS.Models
+{
+    public class Coupon
+    {
+        public string Code { get; set; }
+        public bool IsValid { get { return Validate(this); } }
+        public DateTime ExpireDate { get; set; }
+        public bool IsExpired { get { return DateTime.Today <= ExpireDate; } }
+
+        public static Coupon NewCoupon()
+        {
+            return new Coupon { Code = Guid.NewGuid().ToString("N") };
+        }
+
+        public static bool Validate(Coupon c)
+        {
+            Guid result;
+
+            return Guid.TryParseExact(c.Code, "N", out result);
+        }
+    }
+}
---
+++ POS/POS/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace POS.Models
+{
+    public class Product
+    {
+        public string ID { get; set; }
+        public decimal Price { get; set; }
+    }
+}
