5cd2e1379c891eadb28cd7042ddec1f62262209c
Chris <cmagerabronn110@web.de>
2015-04-05 12:38:26 +0200

    added single instance

--- POS/POS/Program.cs
+++ POS/POS/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows.Forms;
 using Telerik.WinControls;
 
@@ -7,18 +10,40 @@ namespace POS
 {
     static class Program
     {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-
             ThemeResolutionService.ApplicationThemeName = new Telerik.WinControls.Themes.TelerikMetroTouchTheme().ThemeName;
 
-            Application.Run(new SplashScreen());
+            bool createdNew = true;
+            using (Mutex mutex = new Mutex(true, Application.ProductName, out createdNew))
+            {
+                if (createdNew)
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new SplashScreen());
+                }
+                else
+                {
+                    Process current = Process.GetCurrentProcess();
+                    foreach (Process process in Process.GetProcessesByName(current.ProcessName))
+                    {
+                        if (process.Id != current.Id)
+                        {
+                            SetForegroundWindow(process.MainWindowHandle);
+                            break;
+                        }
+                    }
+                }
+            }
         }
     }
 }
