84d2271a558f77643b91d82d49c1a57f123536dd
Chris <cmagerabronn110@web.de>
2015-04-13 19:58:08 +0200

    added testplugin architecture

---
+++ POS/Plugins/TestPlugin/TestPlugin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using POS.Contracts;
+using POS.Contracts.Architecture;
+
+namespace TestPlugin
+{
+    [PlugIn("TestPlugin")]
+    public class TestPlugin : PlugIn<IPosApplication>, IPosPlugIn
+    {
+        public void Load()
+        {
+            
+        }
+    }
+}
