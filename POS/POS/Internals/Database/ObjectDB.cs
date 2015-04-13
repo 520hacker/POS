009556cceef943005df6a7fce0320547482547b2
Chris <cmagerabronn110@web.de>
2015-04-13 19:51:55 +0200

    currency change

---
+++ POS/POS/Internals/Database/ObjectDB.cs
@@ -0,0 +1,10 @@
+namespace POS.Internals.Database
+{
+    public class ObjectDB
+    {
+        public void Append(string path, object obj)
+        {
+
+        }
+    }
+}
--- POS/POS/Models/Invoice.cs
+++ POS/POS/Models/Invoice.cs
@@ -7,10 +7,17 @@ namespace POS.Models
 {
     public class Invoice
     {
-        public enum InvoiceCurrency
+        public class InvoiceCurrency
         {
-            BTC,
-            EUR
+            private readonly string name;
+
+            public InvoiceCurrency(string name)
+            {
+                this.name = name;
+            }
+
+            public static InvoiceCurrency EUR { get { return new InvoiceCurrency("EUR"); } }
+            public static InvoiceCurrency BTC { get { return new InvoiceCurrency("BTC"); } }
         };
 
         public string ID { get; set; }
