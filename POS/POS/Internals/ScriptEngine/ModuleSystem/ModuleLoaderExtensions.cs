b8903b9b6e9685473d7f804f4e0417579293f1d1
Chris <cmagerabronn110@web.de>
2015-04-14 20:06:43 +0200

    little cleanup

--- POS/POS/Internals/ScriptEngine/ModuleSystem/ModuleLoaderExtensions.cs
+++ POS/POS/Internals/ScriptEngine/ModuleSystem/ModuleLoaderExtensions.cs
@@ -8,35 +8,40 @@ namespace POS.Internals.ScriptEngine.ModuleSystem
 {
     public static class ModuleLoaderExtensions
     {
+        public static void Load(this WindowsScriptEngine se, Type t)
+        {
+            var ca = t.GetCustomAttribute<ScriptModuleAttribute>();
+
+            se.AddHostType(t.Name, t);
+
+            if (ca != null)
+            {
+                foreach (var me in t.GetMethods())
+                {
+                    var meca = me.GetCustomAttribute<ScriptFunctionAttribute>();
+                    if (meca != null)
+                    {
+                        se.AddHostObject(me.Name, Delegate.CreateDelegate(t, me));
+                    }
+                }
+                foreach (var me in t.GetProperties())
+                {
+                    var meca = me.GetCustomAttribute<ScriptMemberAttribute>();
+                    if (meca != null)
+                    {
+                        var tmp = Activator.CreateInstance(t);
+
+                        se.AddHostObject(me.Name, me.GetValue(tmp, null));
+                    }
+                }
+            }
+        }
+
         public static void Load(this WindowsScriptEngine se, Assembly ass)
         {
             foreach (var t in ass.GetTypes())
             {
-                var ca = t.GetCustomAttribute<ScriptModuleAttribute>();
-
-                se.AddHostType(t.Name, t);
-
-                if (ca != null)
-                {
-                    foreach (var me in t.GetMethods())
-                    {
-                        var meca = me.GetCustomAttribute<ScriptFunctionAttribute>();
-                        if (meca != null)
-                        {
-                            se.AddHostObject(me.Name, Delegate.CreateDelegate(t, me));
-                        }
-                    }
-                    foreach (var me in t.GetProperties())
-                    {
-                        var meca = me.GetCustomAttribute<ScriptMemberAttribute>();
-                        if (meca != null)
-                        {
-                            var tmp = Activator.CreateInstance(t);
-
-                            se.AddHostObject(me.Name, me.GetValue(tmp, null));
-                        }
-                    }
-                }
+                Load(se, t);
             }
         }
     }
