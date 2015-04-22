36d5102d7768855e68f85a9bb13536de46de2912
Chris <cmagerabronn110@web.de>
2015-04-22 18:55:39 +0200

    added gettext script module

---
+++ POS/Std/GettextModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using POS.Internals.I18N;
+using Pos.Internals.ScriptEngine.ModuleSystem;
+
+namespace Std
+{
+    [ScriptModule(AsType = false)]
+    public class GettextModule
+    {
+        private static Catalog _catalog = new Catalog();
+
+        [ScriptFunction(Name = "gettext_load")]
+        public static Catalog Load(string filename)
+        {
+            _catalog.Load(filename);
+
+            return _catalog;
+        }
+
+        [ScriptFunction(Name = "gettext_")]
+        public static string GetString(string id)
+        {
+            return _catalog.GetString(id);
+        }
+    }
+}
